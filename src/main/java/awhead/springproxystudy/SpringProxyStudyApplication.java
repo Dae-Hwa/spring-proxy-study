83de71fdb743e447549495b485e005880b25376a
Dae-Hwa <dae_hwa@deali.net>
2025-03-13 11:17:04 +0900

    init

---
+++ src/main/java/awhead/springproxystudy/SpringProxyStudyApplication.java
@@ -0,0 +1,13 @@
+package awhead.springproxystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProxyStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProxyStudyApplication.class, args);
+	}
+
+}
